da541a00200cde7b47c4f623475588a94471765c
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2019-10-12 19:09:10 +0300

    Update Fractalysis for new host path

--- nen/fractalysis/service-worker.js
+++ nen/fractalysis/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7beba640294e8aab9d846a541b9bc88f.js"
+  "/nen/fractalysis/precache-manifest.f1de15c5ca3fea298f554133e5a3c8cf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/nen/fractalysis/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
